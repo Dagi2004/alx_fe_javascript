ea45494c4e9f432e04b800e5285208d30b7bb2a9
Dagi <dagmawimilkias@gmail.com>
2024-07-25 21:15:13 +0300

    Exporting and importing JSON files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,7 +3,9 @@ const showQuoteButton = document.getElementById("newQuote");
 const formContainer = document.getElementById("formContainer");
 const inputText = document.getElementById("newQuoteText");
 const categoryInput = document.getElementById("newQuoteCategory");
-const quote = [
+const exportButton = document.getElementById("exportButton");
+const importButton = document.getElementById("importFile");
+let quotes = [
   {
     quoteText:
       "You know the old saying: you win some, you lose some... and then there's that little-known third category.",
@@ -16,11 +18,16 @@ const quote = [
   },
 ];
 
+// load to local storage
+const storedQuote = localStorage.getItem("quotes");
+if (storedQuote) {
+  quotes = JSON.parse(storedQuote);
+}
 // console.log(randomQuote);
 
 function showRandomQuote() {
-  let randomQuote = Math.floor(Math.random() * quote.length);
-  let quotefromArray = quote[randomQuote];
+  let randomQuote = Math.floor(Math.random() * quotes.length);
+  let quotefromArray = quotes[randomQuote];
   quoteDisplay.innerHTML = `<p>Quote:${quotefromArray.quoteText}</p>
   <p>Category:${quotefromArray.category}</p>`;
 }
@@ -28,12 +35,13 @@ function addQuote() {
   const newQuote = document.getElementById("newQuoteText").value;
   const newCategory = document.getElementById("newQuoteCategory").value;
   if (newQuote && newCategory) {
-    quote.push({ quoteText: newQuote, category: newCategory });
+    quotes.push({ quoteText: newQuote, category: newCategory });
     document.getElementById("newQuoteText").value = "";
     document.getElementById("newQuoteCategory").value = "";
+    localStorage.setItem("quotes", JSON.stringify(quotes));
     alert("new quote added");
   }
-  console.log(quote);
+  // console.log(quote);
 }
 function createAddQuoteForm() {
   const form = document.createElement("div");
@@ -59,5 +67,29 @@ function createAddQuoteForm() {
   formContainer.appendChild(form);
 }
 
+function exportJSON() {
+  const jsonData = JSON.stringify(quotes);
+  const blob = new Blob([jsonData], { type: "application.json" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "quotes.json";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    localStorage.setItem("quotes", JSON.stringify(quotes));
+    alert("Quotes imported successfully!");
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
+
 showQuoteButton.addEventListener("click", showRandomQuote);
 createAddQuoteForm();
+exportButton.addEventListener("click", exportJSON);
+importButton.addEventListener("onchange", importFromJsonFile);
